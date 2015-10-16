fae9f92534c574a5143e885c281231ea80dff77a
EuniceMaye Ruiz <eunice@g-angle.co.jp>
2015-10-16 10:12:55 +0800

    first commit

---
+++ pepperController/js/control.js
@@ -0,0 +1,24 @@
+var self = this;
+
+function connect(){
+  var pepperIP = $("#pepperIP").val();
+
+  var setupIns_ = function(){
+    self.session.service("ALTextToSpeech").done(function(ins){
+      self.tts = ins;
+    });
+  }
+  self.session = new QiSession(pepperIP);
+  self.session.socket().on('connect', function(){
+    self.session.service("ALTextToSpeech").done(function(tts){
+      tts.say("Connected");
+    });
+    // document.getElementById('connectionStatus').text = "Connected";
+  }).on('disconnect', function(){
+    alert("disconnected");
+  });
+
+
+
+
+}
