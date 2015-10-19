fcd0a9e1a30611da5fcc20e33759867bcb21d3b0
EuniceMaye Ruiz <eunice@g-angle.co.jp>
2015-10-19 09:21:24 +0800

    changes

--- pepperController/js/control.js
+++ pepperController/js/control.js
@@ -1,11 +1,27 @@
 var self = this;
 
 function connect(){
-  var pepperIP = $("#pepperIP").val();
+  var pepperIP = document.getElementsByName("pepperIP")[0].value;
 
   var setupIns_ = function(){
     self.session.service("ALTextToSpeech").done(function(ins){
       self.tts = ins;
+    });
+
+    self.session.service("ALMotion").done(function(ins){
+      self.alMotion =ins;
+    });
+
+    self.session.service("ALBehaviorManager").done(function(ins){
+      self.alBehavior = ins;
+    });
+
+    self.session.service("ALAnimatedSpeech").done(function(ins){
+      self.alAnimatedSpeech = ins;
+    });
+
+    self.session.service("ALRobotPosture").done(function(ins){
+      self.alRobotPosture = ins;
     });
   }
   self.session = new QiSession(pepperIP);
@@ -13,12 +29,73 @@ function connect(){
     self.session.service("ALTextToSpeech").done(function(tts){
       tts.say("Connected");
     });
-    document.getElementById('connectionStatus').text = "Connected";
+    setupIns_();
+
+    window.location = "controlPepper.html";
+
   }).on('disconnect', function(){
     alert("disconnected");
   });
+}
+function say(){
+  console.log("say function initiated.");
+  var text = document.getElementsByName("sayText")[0].value;
+  this.tts.say(text);
+}
 
+function move(to){
+  if(self.alMotion){
+    console.log("move function initiated.");
+    switch (to) {
+      case 0:
+          //move forward
+          self.alMotion.moveTo(0.3, 0, 0).fail(function(err){console.log(err);});
+          break;
 
+      case 1:
+          //face left
+          self.alMotion.moveTo(0, 0, 0.5).fail(function(err){
+            console.log(err);
+          });
+  				break;
 
+      case 2:
+          //move backward
+          self.alMotion.moveTo(-0.3, 0, 0).fail(function(err){
+            console.log(err);
+          });
+  				break;
 
+      case 3:
+          //face right
+          self.alMotion.moveTo(0, 0, -0.5).fail(function(err){
+            console.log(err);
+          });
+  				break;
+
+      default:
+          self.alMotion.moveTo(0, 0, 0).fail(function(err){
+            console.log(err);
+          });
+          break;
+    }
+  }
+}
+
+function standGesture(){
+console.log("stand function initiated");
+  if(self.alMotion){
+    console.log("if");
+    self.alRobotPosture.goToPosture("Stand", 0.8);
+    // self.alBehavior.runBehavior("animations/Stand/Emotions/Positive/Laugh_1");
+  }
+}
+
+function handsUp(){
+  connect();
+  console.log("hands up initiated");
+  if(self.alBehavior){
+    console.log("if");
+    self.alBehavior.runBehavior("animations/Stand/Gestures/ShowSky_8");
+  }
 }
